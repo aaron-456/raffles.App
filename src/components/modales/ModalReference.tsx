c6bc58f22c3297d805e2fb38a5f56c5502f10ba1
aaron-456 <aaronburbanoms15@gmail.com>
2023-12-29 21:33:27 -0500

    consume enpoint post in modal form

--- src/components/modales/ModalReference.tsx
+++ src/components/modales/ModalReference.tsx
@@ -3,7 +3,7 @@ import './styles/modalReference.css'
 
 const ModalReference = () => {
     return (
-        <div className="modal fade modal_" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal fade modal_" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div className="modal-dialog">
                 <div className="modal-content body_modal">
                         
