1e1b2a516ed56f8f23371345f3bf58258c31fc40
aaron-456 <aaronburbanoms15@gmail.com>
2024-01-07 23:49:04 -0500

    add responsive images and a carousel

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,23 @@ import Reference_section from "./components/reference_section";
 import ShoppingCart_Section from "./components/shoppingCart_Section";
 import { useBoolean } from "./context/MiContextoProvider";
 
-
-
 function App() {
   const { isBoolean } = useBoolean();
   return (
     <>
       <div className="app">
-        {
-          !isBoolean ?
-            <>
-              <MainSection />
-              <GeneralInformation_Section />
-              <ShoppingCart_Section />
-              <FoodPage_section />
-            </> 
-            :
-            <div>
-              <Reference_section />  
-            </div>
-        }
+        {!isBoolean ? (
+          <>
+            <MainSection />
+            <GeneralInformation_Section />
+            <ShoppingCart_Section />
+            <FoodPage_section />
+          </>
+        ) : (
+          <div>
+            <Reference_section />
+          </div>
+        )}
       </div>
     </>
   );
--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -1,15 +1,74 @@
-import React from "react";
-import PercentageBar from "./PercentageBar";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 import "./styles/mainSection.css";
+import React, { useState, useEffect } from "react";
+import { Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import PercentageBar from "./PercentageBar";
 
 const MainSection = () => {
+  const [isMobile, setIsMovile] = useState(window.innerWidth < 600);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMovile(window.innerWidth < 600);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <section className="main__section">
-      <h1 className="title__app">GRAN SORTEO</h1>
+      <h1 className="title__app">GRAN SORTE0</h1>
       <PercentageBar />
-      <div className="raffleImg__box">
-        <img src="" alt="" />
-      </div>
+
+      {isMobile ? (
+        <div className="raffleImg__box">
+          <Swiper
+            modules={[Pagination]}
+            spaceBetween={50}
+            slidesPerView={1}
+            pagination={{ clickable: true }}
+            onSlideChange={() => console.log("slide change")}
+          >
+            <SwiperSlide>
+              {" "}
+              <img
+                className="img_raflle"
+                src="./image2.jpg"
+                alt="img not found"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className="img_raflle"
+                src="./imagen3.jpg"
+                alt="img not found"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              {" "}
+              <img
+                className="img_raflle"
+                src="./imagen4.jpg"
+                alt="img not found"
+              />
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      ) : (
+        <div className="raffleImg__box">
+          <img src="./image2.jpg" alt="img not found" />
+          <img src="./imagen3.jpg" alt="img not found" />
+          <img src="./imagen4.jpg" alt="img not found" />
+        </div>
+      )}
     </section>
   );
 };
